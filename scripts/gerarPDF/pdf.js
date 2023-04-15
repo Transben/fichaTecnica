941b5baed26656726f311ada6b289d8b6febcfc2
Isaque Lopes <isaque.lopes047@gmail.com>
2023-04-15 11:55:25 -0300

    add

--- scripts/gerarPDF/pdf.js
+++ scripts/gerarPDF/pdf.js
@@ -13,14 +13,15 @@ function gerarPDF() {
     doc.addImage(imgData, 'JPEG', 10, 10, 90, 20);
     doc.text("ENTREGA TÉCNICA TRANSBEN", 60, 50);
 
-    doc.text(`Eu ${localStorage.getItem('nomeMotorista')} declaro ter recebido a frota ${localStorage.getItem('frota')} e fui devidamente orientado pelo`, 10, 70);
-    doc.text(`instrutor ${localStorage.getItem('instrutor')}, a respeito dos seguintes temas:`, 10, 77);
+    doc.text(`Eu ${localStorage.getItem('frota')}`, 10, 70);
+    doc.text(`Declaro ter recebido a frota ${localStorage.getItem('frota')} e fui devidamente orientado pelo`, 10, 77);
+    doc.text(`instrutor ${localStorage.getItem('instrutor')}, a respeito dos seguintes temas:`, 10, 84);
 
     // Define a fonte e o estilo "oblique" para todos os textos
     doc.setFontSize(10);
 
     // Adiciona uma lista de itens com 5 pontos de espaço entre cada item
-    var y = 85; // define a posição y inicial
+    var y = 95; // define a posição y inicial
     var itens = localStorage.getItem('segmento').split(',');
     for (var i = 0; i < itens.length; i++) {
         doc.text(" | " + itens[i], 10, y);
