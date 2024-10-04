ae740b3994cfec2856b7746242885e908553db2b
Isaquelopes047 <isaque.lopes047@gmail.com>
2024-10-04 13:38:14 -0300

    atualizado, criado novas opções

--- scripts/addArray.js
+++ scripts/addArray.js
@@ -1,7 +1,17 @@
 /* GERA O ARRAY INICIAL DE OPÇÕES PADROES NA INICIALIZAÇÃO DA PAGINA */
 
 function getCaminhao(){
-    var Volvo = ["PNEU","PREENCHIMENTO DO TACÓGRAFO","KM DE REVISÕES","MÉDIA","TANQUE PRINCIPAL E AUXILIAR","BUJÕES DE AR","FREIO MOTOR","PILOTO AUTOMÁTICO","CONFIGURAÇÕES DO COMPUTADOR DE BORDO","EQUIPAMENTO REBOQUE",]
+    var Volvo = [
+        "PNEU",
+        "PREENCHIMENTO DO TACÓGRAFO",
+        "KM DE REVISÕES","MÉDIA",
+        "TANQUE PRINCIPAL E AUXILIAR",
+        "BUJÕES DE AR",
+        "FREIO MOTOR",
+        "PILOTO AUTOMÁTICO",
+        "CONFIGURAÇÕES DO COMPUTADOR DE BORDO",
+        "EQUIPAMENTO REBOQUE",
+    ]
     localStorage.setItem('segmento', JSON.stringify(Volvo))
 }
 getCaminhao()
--- scripts/gerarPDF/pdf.js
+++ scripts/gerarPDF/pdf.js
@@ -2,10 +2,8 @@ import { imgData } from '../../scripts/imgBase64Logo/logo65.js';
 import { dataAtual } from '../../scripts/dataAtual.js';
 
 function gerarPDF() {
-
-    let assinaturaMotosita = localStorage.getItem('assinaturaMotorista')
-    let assinaturaInstrutor = localStorage.getItem('assinaturaInstrutor')
-
+    let assinaturaMotosita = localStorage.getItem('assinaturaMotorista');
+    let assinaturaInstrutor = localStorage.getItem('assinaturaInstrutor');
     let data = dataAtual();
 
     let doc = new jsPDF();
@@ -13,20 +11,20 @@ function gerarPDF() {
     doc.addImage(imgData, 'JPEG', 10, 10, 90, 20);
     doc.text("ENTREGA TÉCNICA TRANSBEN", 60, 50);
 
-    doc.text(`Eu ${localStorage.getItem('nomeMotorista')}`, 10, 70);
-    doc.text(`Declaro ter recebido a frota ${localStorage.getItem('frota')} e fui devidamente orientado pelo`, 10, 77);
-    doc.text(`instrutor ${localStorage.getItem('instrutor')}, a respeito dos seguintes temas:`, 10, 84);
+    doc.text(`Eu, ${localStorage.getItem('nomeMotorista')},`, 10, 70);
+    doc.text(`declaro ter recebido o veículo da frota ${localStorage.getItem('frota')} e ter sido devidamente orientado pelo`, 10, 77);
+    doc.text(`instrutor ${localStorage.getItem('instrutor')} a respeito dos seguintes temas:`, 10, 84);
 
     // Define a fonte e o estilo "oblique" para todos os textos
     doc.setFontSize(10);
 
-    // Adiciona uma lista de itens com 5 pontos de espaço entre cada item
-    var y = 95; // define a posição y inicial
-    var itens = localStorage.getItem('segmento').split(',');
-    for (var i = 0; i < itens.length; i++) {
-        doc.text(" | " + itens[i], 10, y);
-        y += 7; // adiciona 5 pontos à posição y para o próximo item
-    }
+    // Pega a string do storage e faz o parse para transformar em array
+    var itens = JSON.parse(localStorage.getItem('segmento')); // Agora é um array de itens
+    var listaItens = itens.join(', '); // Junta os itens em uma string única, separados por vírgula
+
+    // Adiciona a lista de itens no PDF sem as aspas e colchetes
+    var textOptions = doc.splitTextToSize(listaItens, 190); // Ajusta a largura máxima do texto (190 para manter margens)
+    doc.text(textOptions, 10, 95); // Adiciona o texto ajustado
 
     doc.addImage(assinaturaMotosita, 'JPEG', 10, 220, 50, 50);
     doc.text(`${localStorage.getItem('nomeMotorista')}`, 35, 265);
@@ -38,9 +36,10 @@ function gerarPDF() {
 
     doc.text(`${data}`, 95, 260);
     doc.text("Data", 100, 265);
-    
+
     doc.save(`termoFrota(${localStorage.getItem('frota')})-(${data})`);
 }
 
 var botaoPDF = document.querySelector('.gerarPDFButton');
+
 botaoPDF.addEventListener('click', gerarPDF);
